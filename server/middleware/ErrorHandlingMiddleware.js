923a256f9fa7510673e44a1a988892af8c88df07
Anton <toxajlb@yandex.ru>
2023-02-01 20:01:33 +0300

    Finished backend part

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -1,8 +1,8 @@
 const ApiError = require('../error/ApiError');
 
-module.exports = function(error, req, res, next) {
-    if (error instanceof ApiError) {
-        return res.status(error.status).json({message: error.message});
+module.exports = function(err, req, res, next) {
+    if (err instanceof ApiError) {
+        return res.status(err.status).json({message: err.message});
     }
     return res.status(500).json({message: "Непредвиденная ошибка"});
 }
