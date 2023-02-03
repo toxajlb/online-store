6ebd603fce9c09794d47db8f8b35e10ecfa50d00
Anton <toxajlb@yandex.ru>
2023-02-03 16:31:32 +0300

    Worked on Navbar and Auth components

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,10 +1,8 @@
 import { useContext } from "react";
 import { Context } from "..";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from "react-router-dom";
 import { SHOP_ROUTE } from "../utils/consts";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
 
 const NavBar = observer( () => {
--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,10 +1,40 @@
+import { Button, Card, Container, Form } from "react-bootstrap";
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE
     
     return (
-        <>
-          Auth  
-        </>
+        <Container 
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}>
+               <Card style={{width: 600}} className="p-5">
+                    <h2 className="m-auto">Авторизация</h2>
+                    <Form className="d-flex flex-column">
+                        <Form.Control className="mt-3" placeholder="Введите e-mail..."/>
+                        <Form.Control className="mt-3" placeholder="Введите пароль..."/>
+                        <Form className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрироваться!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        }
+                        <Button
+                            variant={"outline-success"}
+                        >
+                            {isLogin ? 'Войти' : 'Регистрация'}
+                        </Button>
+                    </Form>
+                    </Form>
+               </Card> 
+
+        </Container>
     )
 }
 
