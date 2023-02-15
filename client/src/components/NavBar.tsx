fb7501d9e2ed1f5b9e6b766e720b06d3d7018ce6
Anton <toxajlb@yandex.ru>
2023-02-15 17:01:36 +0300

    added some changes

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Context } from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, BASKET_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { Button } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
@@ -16,6 +16,7 @@ const NavBar = observer(() => {
     const logOut = () => {
         user.setUser({});
         user.setIsAuth(false);
+        navigate(SHOP_ROUTE);
     }
 
     return (
@@ -29,6 +30,13 @@ const NavBar = observer(() => {
                             onClick={() => navigate(ADMIN_ROUTE)}
                         >
                             Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(BASKET_ROUTE)}
+                            style={{marginLeft: '10px'}}
+                        >
+                            Корзина
                         </Button>
                         <Button
                             variant={"outline-light"}
